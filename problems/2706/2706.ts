b1e80da3df114185f77d13982174c6bd3c902837
Eber Alejo <eber4202@hotmail.com>
2025-08-27 21:51:47 -0600

    refactor(2706): reduce algorithm length

--- problems/2706/2706.ts
+++ problems/2706/2706.ts
@@ -1,19 +1,5 @@
 export function buyChoco(prices: number[], money: number): number {
-  let bought = 0
   prices.sort((a, b) => a - b)
-
-  let chocos = 0
-  const MAX_CHOCOS = 2
-  for (let i = 0; i < prices.length; i++) {
-    if (chocos === MAX_CHOCOS) break
-
-    const price = prices[i]
-    if (price < money && bought + price <= money) {
-      bought += price
-      chocos += 1
-    }
-  }
-
-  if (chocos < MAX_CHOCOS) return money
-  return money - bought
+  const firstTwoChocos = prices[0] + prices[1]
+  return firstTwoChocos <= money ? money - firstTwoChocos : money
 }
