236825bd6385632ec5f38f93e9cf0e6a3fb17234
Eber Alejo <eberealejo@gmail.com>
2026-06-22 20:45:43 -0600

    feat(problems/2): add solution for 2. Add Two Numbers

---
+++ problems/2/2.ts
@@ -0,0 +1,23 @@
+import { ListNode } from './types';
+
+export function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {
+  let carry = 0;
+  const dummyHead = new ListNode();
+  let pointer = dummyHead;
+
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    const val1 = l1?.val ?? 0;
+    const val2 = l2?.val ?? 0;
+    const sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+    pointer.next = new ListNode(sum % 10);
+    pointer = pointer.next;
+    l1 = l1?.next ?? null;
+    l2 = l2?.next ?? null;
+  }
+
+  return dummyHead.next;
+}
